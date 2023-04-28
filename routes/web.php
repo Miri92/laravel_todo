f0313539348798286eae8390cbb357cc4ea7d87d
miri <miri.zulfugar@gmail.com>
2023-04-28 10:42:34 +0400

    .

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+//test
 Route::group(['middleware' => ['web','auth']], function () {
     Route::get('/home', 'TodoController@index');
     Route::get('todo/list', 'TodoController@index')->name('todo.list');
